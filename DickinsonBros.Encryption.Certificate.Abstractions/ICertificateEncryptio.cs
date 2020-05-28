d52d573b209a57b73ba6695b3d2ee0a28e740637
Mark Dickinson <marksamdickinson@gmail.com>
2020-05-27 22:26:13 -0700

    Add project files.

---
+++ DickinsonBros.Encryption.Certificate.Abstractions/ICertificateEncryptio.cs
@@ -0,0 +1,8 @@
+﻿namespace DickinsonBros.Encryption.Certificate.Abstractions
+{
+    public interface ICertificateEncryption<T>
+    {
+        string Decrypt(byte[] encrypted);
+        byte[] Encrypt(string rawString);
+    }
+}
