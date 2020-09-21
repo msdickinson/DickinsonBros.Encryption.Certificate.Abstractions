3a3bc89c1d9f1382675f771fcc4271ffcefa182e
Mark Dickinson <marksamdickinson@gmail.com>
2020-09-20 22:54:43 -0700

    update

--- Scripts/CreateNugetPackageCI.ps1
+++ Scripts/CreateNugetPackageCI.ps1
@@ -1,4 +1,4 @@
 $Name = "DickinsonBros.Encryption.Certificate.Abstractions"
 $DateTime = [datetime]::UtcNow.ToString("yyyyMMdd-HHmmss")
 $VersionSuffix = "-alpha" + $DateTime
-dotnet pack $Name --version-suffix $VersionSuffix --output C:\Packages
+dotnet pack $Name -c Release --version-suffix $VersionSuffix --output C:\Packages
